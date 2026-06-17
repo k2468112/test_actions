9241733947795d94ccd01ad0543d8e7d49baeee9
k2468112 <k6325041@gmail.com>
2026-06-16 19:22:40 -0500

    run again

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import json
 from os import getenv
-import os
+import os, json
 from collections.abc import Callable
 from dotenv import load_dotenv
 
 load_dotenv()
 print("!!!")
-print(os.environ)
+# print(os.environ)
+print("GSHEET_LOG_IDS" , getenv("GSHEET_LOG_IDS"))
+print("****")
+print("GOOGLE_SERVICEKEY_JSON" , getenv("GOOGLE_SERVICEKEY_JSON"))
+print("****")
+print("****")
+print("****")
+print("****")
+print("****")
+print("****")
+print("****")
+print("DICTIONARIES???")
+print("GSHEET_LOG_IDS" , json.loads(getenv("GSHEET_LOG_IDS")))
+print("GOOGLE_SERVICEKEY_JSON" , json.loads(getenv("GOOGLE_SERVICEKEY_JSON")))
